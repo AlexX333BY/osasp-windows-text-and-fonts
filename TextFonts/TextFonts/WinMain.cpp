599f4ea4ef5ba8abdbd0024280df14b55879f286
Aliaksei <alexx333by@gmail.com>
2018-10-16 16:00:53 +0300

    Text input implemented

--- TextFonts/TextFonts/WinMain.cpp
+++ TextFonts/TextFonts/WinMain.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <strsafe.h>
 
 namespace defaults
 {
@@ -28,6 +29,19 @@ SIZE GetClientWindowSize(HWND hWnd)
 	return size;
 }
 
+int FillWindowWithColor(HWND hWnd, COLORREF crColor)
+{
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	HDC hWndDC = GetDC(hWnd);
+	HBRUSH hBrush = CreateSolidBrush(crColor);
+	int iResult = FillRect(hWndDC, &rect, hBrush);
+	DeleteObject(hBrush);
+	ReleaseDC(hWnd, hWndDC);
+	return iResult;
+}
+
+
 BOOL UpdateStampPosition(HWND hWnd, COORD &rcStampCoords, SIZE &rsStampSize,
 	BYTE cbLeftIndentPercents = defaults::cbLeftIndentPercents, 
 	BYTE cbRightIndentPercents = defaults::cbRightIndentPercents,
@@ -54,15 +68,91 @@ BOOL UpdateStampPosition(HWND hWnd, COORD &rcStampCoords, SIZE &rsStampSize,
 	return true;
 }
 
+LPTSTR GetEmptyString()
+{
+	LPTSTR newEmptyText = (LPTSTR)(calloc(1, sizeof(TCHAR)));
+	newEmptyText[0] = '\0';
+	return newEmptyText;
+
+}
+
+LPTSTR DeleteLastChar(LPTSTR lpsText)
+{
+	int iTextLength = lstrlen(lpsText);
+	if (iTextLength == 0)
+	{
+		return GetEmptyString();
+	}
+	else
+	{
+		LPTSTR lpsNewText = (LPTSTR)(calloc(iTextLength, sizeof(TCHAR)));
+		if (StringCchCopyN(lpsNewText, iTextLength, lpsText, iTextLength - 1) == S_OK)
+		{
+			return lpsNewText;
+		}
+		else
+		{
+			free(lpsNewText);
+			return NULL;
+		}
+	}
+}
+
+LPTSTR ConcatStringAndChar(LPTSTR lpsText, TCHAR cChar)
+{
+	int iNewLength = lstrlen(lpsText) + 2;
+	LPTSTR lpsNewText = (LPTSTR)(calloc(iNewLength, sizeof(TCHAR)));
+	if (StringCchCopy(lpsNewText, iNewLength, lpsText) == S_OK)
+	{
+		lpsNewText[iNewLength - 2] = cChar;
+		lpsNewText[iNewLength - 1] = '\0';
+		return lpsNewText;
+	}
+	else
+	{
+		free(lpsNewText);
+		return NULL;
+	}
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	static COORD coordStamp = { 0 };
 	static SIZE sizeStamp = { 0 };
+	static LPTSTR lpsText = GetEmptyString();
 
 	switch (message)
 	{
+	case WM_CREATE:
+		DefWindowProc(hWnd, message, wParam, lParam);
+		break;
 	case WM_SIZE:
 		UpdateStampPosition(hWnd, coordStamp, sizeStamp);
+		DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	case WM_PAINT:
+		FillWindowWithColor(hWnd, GetBackgroundColor());
+		break;
+	case WM_CHAR:
+		LPTSTR newText;
+		switch (wParam)
+		{
+		case VK_BACK:
+			if ((newText = DeleteLastChar(lpsText)) != NULL)
+			{
+				free(lpsText);
+				lpsText = newText;
+			}
+		case VK_RETURN:
+		case VK_ESCAPE:
+			break;
+		default:
+			if ((newText = ConcatStringAndChar(lpsText, (TCHAR)wParam)) != NULL)
+			{
+				free(lpsText);
+				lpsText = newText;
+			}
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
