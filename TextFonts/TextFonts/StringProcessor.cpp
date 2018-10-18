b72bce3f8376b4ad7b83e7bcf7ef469ab3f6a4c6
Aliaksei <alexx333by@gmail.com>
2018-10-19 00:46:49 +0300

    StringProcessor class implemented

---
+++ TextFonts/TextFonts/StringProcessor.cpp
@@ -0,0 +1,69 @@
+#include <strsafe.h>
+#include "StringProcessor.h"
+
+namespace Stamp
+{
+	LPTSTR StringProcessor::CopyString(LPTSTR lpsOriginalString)
+	{
+		if (lpsOriginalString == NULL)
+		{
+			return NULL;
+		}
+		int iStringCopyLength = lstrlen(lpsOriginalString);
+		LPTSTR lpsStringCopy = (LPTSTR)calloc(iStringCopyLength + 1, sizeof(TCHAR));
+		if (StringCchCopy(lpsStringCopy, iStringCopyLength + 1, lpsOriginalString) == S_OK)
+		{
+			return lpsStringCopy;
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+
+	LPTSTR StringProcessor::GetEmptyString()
+	{
+		LPTSTR lpsEmptyString = (LPTSTR)calloc(1, sizeof(TCHAR));
+		lpsEmptyString[0] = '\0';
+		return lpsEmptyString;
+	}
+
+	LPTSTR StringProcessor::DeleteLastChar(LPTSTR lpsOriginalString)
+	{
+		int iStringLength = lstrlen(lpsOriginalString);
+		if (iStringLength == 0)
+		{
+			return GetEmptyString();
+		}
+		else
+		{
+			LPTSTR lpsNewString = (LPTSTR)calloc(iStringLength, sizeof(TCHAR));
+			if (StringCchCopyN(lpsNewString, iStringLength, lpsOriginalString, iStringLength - 1) == S_OK)
+			{
+				return lpsNewString;
+			}
+			else
+			{
+				free(lpsNewString);
+				return NULL;
+			}
+		}
+	}
+
+	LPTSTR StringProcessor::AddChar(LPTSTR lpsOriginalString, TCHAR cCharToAdd)
+	{
+		int iNewLength = lstrlen(lpsOriginalString) + 2;
+		LPTSTR lpsNewString = (LPTSTR)calloc(iNewLength, sizeof(TCHAR));
+		if (StringCchCopy(lpsNewString, iNewLength, lpsOriginalString) == S_OK)
+		{
+			lpsNewString[iNewLength - 2] = cCharToAdd;
+			lpsNewString[iNewLength - 1] = '\0';
+			return lpsNewString;
+		}
+		else
+		{
+			free(lpsNewString);
+			return NULL;
+		}
+	}
+}
---
+++ TextFonts/TextFonts/StringProcessor.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <windows.h>
+
+namespace Stamp
+{
+	static class StringProcessor
+	{
+	public:
+		static LPTSTR CopyString(LPTSTR lpsOriginalString);
+		static LPTSTR GetEmptyString();
+		static LPTSTR DeleteLastChar(LPTSTR lpsOriginalString);
+		static LPTSTR AddChar(LPTSTR lpsOriginalString, TCHAR cCharToAdd);
+	};
+}
